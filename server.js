a369d330aa11bdb45adfb5b5d4781b7d354bf83a
Dan Proctor <dproc96@gmail.com>
2019-10-29 20:02:05 -0400

    initial site files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/chefd";
+
+mongoose.connect(MONGODB_URI);
+
+require("./routes/apiRoutes")(app);
+
+const root = path.join(__dirname, 'client', 'build')
+app.use(express.static(root));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
