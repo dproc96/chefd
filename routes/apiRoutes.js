556cee94c4dae0bbcb0307aee05f3000b293a482
ngiridhar1 <50300536+ngiridhar1@users.noreply.github.com>
2019-11-08 19:07:14 -0500

    Adding favourite recipies post in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -70,14 +70,20 @@ module.exports = app => {
   });
 
   app.post("/api/account/favouriterecipes", auth, async(req,res)=>{
-    //create the association of the Useraccount with the user from auth
-    const userAccount = new db.UserAccount({
+    //create the association of the Favourite recipes with the user from auth
+    const profile = new db.Profile({
       ...req.body,
       owner:req.user._id
     })
+    db.Profile.create(req.body, (error, data) => {
+      if (error) {
+        response.status(503).end();
+      }
+      response.json({message: "Creating user"});
+    });
     try {
-      await userAccount.save()
-      res.status(201).send(userAccount)
+      await profile.save()
+      res.status(201).send(profile)
     } catch (e) {
       res.status(400).send(e)
     }
